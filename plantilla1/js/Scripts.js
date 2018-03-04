a43cf7f311156e1cfc6f959b7aed7de86feac826
Unknown <ulises203@msn.com>
2018-03-04 12:47:07 +0100

    actualizando

--- plantilla1/js/Scripts.js
+++ plantilla1/js/Scripts.js
@@ -12,3 +12,17 @@ window.onload=function(){
     alert("Bienvenida");
 }
 
+function agranda(){
+    element=document.getElementById("verde");
+    styleel=document.defaultView.getComputedStyle(element,null); // segundo parametro ej:null, "hover", "first-letter" taicionero con hover  
+    //if (parseInt(styleel.height)<300){
+    element.style.height=(parseInt(styleel.height)+5)+'px';
+    //}
+    //else{
+       // console.log(parseInt(styleel.height));
+       // document.getElementById("verde").innerHTML = '<p>TAS PASAU!</p>';
+        
+    //}
+        
+    
+    }
